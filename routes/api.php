2a9d1d54f05f8055c4746ff759cab2e0a8565b89
abdulrahman-kbm <abdulrahman.kbm02@gmail.com>
2024-10-02 01:43:18 +0300

    add ForgotPassword and ResetPassword Controllers

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
+// use App\Http\Controllers\ForgotPasswordController;
+
+use App\Http\Controllers\ForgotPasswordController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ResetPasswordController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +17,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('forgotpassword',ForgotPasswordController::class);
+Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
