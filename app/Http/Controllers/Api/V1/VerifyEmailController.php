0eec7cac28a1d51020f015337568a9ab83b40f98
SourceM7 <mohamadaladlan@yahoo.com>
2024-10-08 12:38:08 +0300

    verify email controller

---
+++ app/Http/Controllers/Api/V1/VerifyEmailController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Exceptions\InvalidCredentialsException;
+use App\Exceptions\UserNotFoundException;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\VerifyEmailRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
+
+class VerifyEmailController extends Controller
+{
+    public function verify(VerifyEmailRequest $request)
+    {
+        $user = User::whereEmail($request->email)->first();
+        if(!$user)
+        {
+            throw new UserNotFoundException();
+        }
+
+        $cachedCode=Cache::get("verification_code_{$user->id}");
+
+        if(!$cachedCode || $cachedCode !==$request->code)
+        {
+            throw new InvalidCredentialsException("Invalid Verification code");
+        }
+
+        $user->email_verified_at=now();
+        $user->save();
+        Cache::forget("verification_code_{$user->id}");
+
+         return response()->json([
+            'message'=>'Email verified successfully'
+        ],200);
+    }
+}
