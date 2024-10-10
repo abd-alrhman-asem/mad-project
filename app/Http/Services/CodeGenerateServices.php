a33a1d3202cce90d1331bffbc32e96638748dcc6
helez22 <tahlohelez@gmail.com>
2024-10-10 10:34:04 +0300

    update2 for register , logout , resendNotification

---
+++ app/Http/Services/CodeGenerateServices.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Services;
+
+use App\Enums\UserStatus;
+use App\Models\User;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+use Carbon\Carbon;
+
+class CodeGenerateServices
+{
+    public function generateCode()
+    {
+        $lowercaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+        $uppercaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $numbers = '0123456789';
+        $characters = $lowercaseLetters . $uppercaseLetters . $numbers;
+        $verificationCode = substr(str_shuffle($characters), 0, 6);
+
+        return $verificationCode;
+    }
+
+    public function storeCodeInCache($userId, $code)
+    {Cache::put("verification_code_{$userId}", $code, 10 * 60);}
+
+    public function verifyCode($userId, $inputCode)
+    {
+        $cachedCode = Cache::get("verification_code_{$userId}");
+        return $cachedCode && $cachedCode === $inputCode;
+    }
+
+
+
+
+
+}
+
